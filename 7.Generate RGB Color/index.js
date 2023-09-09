531ee8266c1232a43804a9b48608b787f297399f
marufbellah <mdmaruf5511507@gmail.com>
2023-09-10 05:02:59 +0600

    Generate RGB color project done

--- 7.Generate RGB Color/index.js
+++ 7.Generate RGB Color/index.js
@@ -3,3 +3,61 @@ Project Requirements :
 -Change the caches,ground color by generating random rgb color by clicking a button  
 
 */
+
+
+// step 1 - create onload handler 
+
+// window.onload =() =>{
+// main()
+// }
+
+// function main(){
+//     const root = document.getElementById('root');
+//     const btn = document.getElementById('change-btn');
+
+
+//     btn.addEventListener('click', function(){
+//     const bgColor =  generatorRGBColor()
+//     root.style.backgroundColor = bgColor;
+
+//     })
+// }
+
+
+window.onload =() => {
+main()
+}
+
+function main(){
+    const root = document.getElementById('root');
+    const btn = document.getElementById('change-btn');
+
+    btn.addEventListener('click', function(){
+        const bgColor = generatorRGBcolor()
+        root.style.backgroundColor = bgColor;
+    })
+}
+
+// step 2 - random color generator function
+
+
+// function generatorRGBColor(){
+//     const red = Math.floor(Math.random() * 255)
+//     const green = Math.floor(Math.random() * 255)
+//     const blue = Math.floor(Math.random() * 255)
+
+//     return `rgb(${red} ,${green}, ${blue})`
+  
+// }
+
+
+function generatorRGBcolor(){
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+// step 3 collect all nessary refarance 
+// generatorRGBColor(54)
