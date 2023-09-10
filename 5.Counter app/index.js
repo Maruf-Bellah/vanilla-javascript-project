421cc8e619b287fd47e1806e804e48c9c4794c4f
marufbellah <mdmaruf5511507@gmail.com>
2023-09-10 21:56:54 +0600

    added condition

--- 5.Counter app/index.js
+++ 5.Counter app/index.js
@@ -2,12 +2,15 @@ var counter = 0;
 
 
 function sub(){
-    counter -= 1;
+    if(counter > 0){
+        counter -= 1;
+    }
     document.getElementById('btn').innerHTML = counter;
     document.getElementById('btn').style='color: red'
 }
 
 function add(){
+
     counter += 1;
     document.getElementById('btn').innerHTML = counter;
     document.getElementById('btn').style='color: #0d6efd'
